bc13420f9261d101181e8a88947c3778fc7b5ecd
Ahsan Ijaz <ahsanijaz6161@gmail.com>
2026-09-11 16:02:43 +0000

    tuning

--- src/train_l232617.py
+++ src/train_l232617.py
@@ -12,6 +12,7 @@ print(data)
 # Features and target
 X = data[["area", "bedrooms", "bathrooms", "stories", "parking"]]
 y = data["price"]
+X = (X - X.mean()) / X.std()
 
 # Train model
 model = LogisticRegression
